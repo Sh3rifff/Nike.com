6e5884abfad9398128a6dd44492bcab200f0119b
Serif <serifsireliyev2002@gmail.com>
2024-05-20 14:44:57 +0400

    First commit

---
+++ app/src/main/java/com/sharif/nikecom/view/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.sharif.nikecom.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.sharif.nikecom.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
